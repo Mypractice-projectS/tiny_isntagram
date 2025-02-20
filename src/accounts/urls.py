08ee152a721c531fed7483782a5f349fc9cc036d
parsafk010 <parsafakharian.f33@gmail.com>
2025-02-20 22:48:48 +0330

    FIX(templates):href links to ensure correct redirection

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -3,6 +3,6 @@ from .views import home , login , Sign_up
 
 urlpatterns = [
     path('', home, name='home'), 
-    path('login', login, name='login'), 
-    path('signup', Sign_up, name='sign-up'), 
+    path('login/', login, name='login'), 
+    path('signup/', Sign_up, name='sign-up'), 
 ]
