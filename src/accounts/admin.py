3ae3622be2f88f09972b45c2eca31f21133e7028
parsafk010 <parsafakharian.f33@gmail.com>
2025-03-05 18:59:07 +0330

    feat:creat admin

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,6 +1,43 @@
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
-from .models import *
+from .forms import UserChangeForm,UserCreationForm
+from .models import User, OTP
+
+
 # Register your models here.
 
-admin.site.register(User)
-admin.site.register(Profile)
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    ordering = ['email']
+    list_display = ['email', 'phone','username','is_active','is_staff','created_at' ,'is_verified']
+    search_fields = ['username']
+    readonly_fields = ['last_login']
+    filter_horizontal = ['groups','user_permissions']
+
+    fieldsets = (
+        ('Main', {'fields':('username','phone', 'email','password')}),
+        ('permissions',{'fields':('is_active','is_staff','is_verified','is_superuser','groups','last_login','user_permissions')}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields':('username','phone','email','password1','password2')}),
+    )
+
+
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        if not is_superuser:
+            form.base_fields['is_superuser'].disabled = True
+        return form
+
+
+admin.site.register(User,UserAdmin)
+
+class OTPAdmin(admin.ModelAdmin):
+    model = OTP
+    list_display = ['user','otp','email']
+
+admin.site.register(OTP,OTPAdmin)
