97dd499611f96805798849f9b5b167932529deec
parsafk010 <parsafakharian.f33@gmail.com>
2025-03-05 18:59:30 +0330

    feat: email otp

--- src/accounts/email.py
+++ src/accounts/email.py
@@ -14,7 +14,7 @@ def send_otp_email(email):
     otp_code = generate_otp()
     print(otp_code)
     user = User.objects.get(email=email)
-    current_site='mohammadrezajavaherykian@gmail.com'
+    current_site='johnfakh223@gmail.com'
     email_body=f'Hi {user} thanks for signing up on {current_site} please verify your email with the \n one time passcode {otp_code}'
     from_email =settings.DEFAULT_FROM_EMAIL
 
