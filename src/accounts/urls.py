2c5ce07de6a4df6dfb53a1cd85d4da1a407864ba
parsafk010 <parsafakharian.f33@gmail.com>
2025-03-05 19:01:34 +0330

    fix: add api/otp

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,14 +1,15 @@
 from django.views.generic import TemplateView
 from django.urls import path
-from .views import HomeView, UserRegisterView, ProfileView, OTPView,UserLoginView
+from .views import HomeView, UserRegisterView, ProfileView, VerifyOTPView,UserLoginView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
-    path('api/login/', UserRegisterView.as_view(), name='login-api'),
+    path('api/login/', UserLoginView.as_view(), name='login-api'),
     path('api/signup/', UserRegisterView.as_view(), name='user-register'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('otp/', OTPView.as_view(), name='otp'),
+    path('api/otp/', VerifyOTPView.as_view(), name='otp-api'),
+    path('otp/', TemplateView.as_view(template_name='otp.html'), name='otp'),
     path('signup/', TemplateView.as_view(template_name='signup.html'), name='signup'),
 
 ]
