1439402853cf16f24dd43cf926e90e99b3c7b0e7
parsafk010 <parsafakharian.f33@gmail.com>
2025-03-08 17:04:08 +0330

    fix:send random code

--- src/accounts/email.py
+++ src/accounts/email.py
@@ -1,9 +1,7 @@
-import random  # برای تولید کد تصادفی OTP
-from django.core.mail import EmailMessage  # برای ارسال ایمیل
-from django.conf import settings  # برای دریافت تنظیمات ایمیل
-from .models import User, OTP  # برای دسترسی به مدل‌های User و OTP
-
-
+from django.core.mail import send_mail,EmailMessage
+import random
+from .models import User,OTP
+from django.conf import settings
 
 def generate_otp():
     return str(random.randint(100000, 999999))
@@ -14,7 +12,7 @@ def send_otp_email(email):
     otp_code = generate_otp()
     print(otp_code)
     user = User.objects.get(email=email)
-    current_site='johnfakh223@gmail.com'
+    current_site='mohammadrezajavaherykian@gmail.com'
     email_body=f'Hi {user} thanks for signing up on {current_site} please verify your email with the \n one time passcode {otp_code}'
     from_email =settings.DEFAULT_FROM_EMAIL
 
@@ -22,3 +20,13 @@ def send_otp_email(email):
 
     d_email = EmailMessage(subject=subject, body=email_body, from_email=from_email, to=[email])
     d_email.send(fail_silently=True)
+
+
+def send_normal_email(data):
+    email=EmailMessage(
+        subject=data['email_subject'],
+        body=data['email_body'],
+        from_email=settings.EMAIL_HOST_USER,
+        to=[data['to_email']],
+    )
+    email.send()
