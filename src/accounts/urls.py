281a46b5caebb6417da077aeaae23d60dc4cffb7
parsafk010 <parsafakharian.f33@gmail.com>
2025-03-08 17:10:14 +0330

    feat:add some url

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,15 +1,30 @@
-from django.views.generic import TemplateView
+from tempfile import template
 from django.urls import path
-from .views import HomeView, UserRegisterView, ProfileView, VerifyOTPView,UserLoginView
+from . import views
+from django.contrib.auth.views import  PasswordChangeView, PasswordChangeDoneView
+from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from rest_framework.authtoken import views as auth_token
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
-    path('api/login/', UserLoginView.as_view(), name='login-api'),
-    path('api/signup/', UserRegisterView.as_view(), name='user-register'),
-    path('profile/', ProfileView.as_view(), name='profile'),
-    path('api/otp/', VerifyOTPView.as_view(), name='otp-api'),
-    path('otp/', TemplateView.as_view(template_name='otp.html'), name='otp'),
+    #home
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    # register
+    path('api/signup/', views.UserRegisterView.as_view(), name='signup-api'),
     path('signup/', TemplateView.as_view(template_name='signup.html'), name='signup'),
+    path('verify_otp/', views.VerifyOTPView.as_view(), name='verify_otp'),
+    #login
+    path('api/login/', views.UserLoginView.as_view(), name='login-api'),
+    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
+    # logout
+    path('logout/', views.LogoutUserView.as_view(), name='logout'),
+    # profile
+    path('api/profile/', views.ProfileView.as_view(), name='profile-api'),
+    path('profile/', TemplateView.as_view(template_name='profile.html'), name='profile'),
+    path('profile/<int:profile_pk>/', views.ProfileView.as_view(), name='profile_get'),
+    # TOKEN JWT
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
