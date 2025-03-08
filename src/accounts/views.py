b3080deedf16a27a2cbee47d46160b468c37c835
parsafk010 <parsafakharian.f33@gmail.com>
2025-03-08 17:11:00 +0330

    feat: creat a logut and profile views

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,13 +1,16 @@
-import random
-from django.views import View
+from typing import Generic
+from rest_framework import status
 from rest_framework.generics import GenericAPIView
-from .models import OTP, User
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from permissions import IsOwnerOrReadOnly
+from .models import User, Profile ,OTP
 from .email import send_otp_email
-from .serializers import UserRegisterSerializer, UserLoginSerializer
-from django.views.generic import TemplateView
+from .serializers import UserRegisterSerializer, UserLoginSerializer, ProfileSerializer, UserUpdateSerializer,LogoutUserSerializer
+# from django.contrib.auth import login
+# from django.utils import timezone
+# from datetime import timedelta
+
 
 
 class UserRegisterView(GenericAPIView):
@@ -16,68 +19,38 @@ class UserRegisterView(GenericAPIView):
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            user = serializer.save()  # ذخیره کاربر در پایگاه داده
-            send_otp_email(user.email)  # ارسال کد OTP به ایمیل
+            serializer.save()
+            user = serializer.data
+            send_otp_email(user['email'])
+            print(user)
+            # send email
             return Response({
-                'message': f'Hi {user.username}, thanks for signing up. A passcode has been sent to your email.'
+                'data':user,
+                'message':f'hi {user} thanks for signing up a passcode'
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 class VerifyOTPView(GenericAPIView):
     def post(self, request):
         otpcode = request.data.get('otp')
-        email = request.data.get('email')
-
         try:
-            # یافتن OTP برای تایید
-            user_code_obj = OTP.objects.get(otp=otpcode, user__email=email)
+            user_code_obj = OTP.objects.get(otp=otpcode)
             user = user_code_obj.user
-
-            # اگر کاربر تایید نشده باشد، وضعیت is_verified را True می‌کنیم
             if not user.is_verified:
                 user.is_verified = True
-                user.save()  # تغییر وضعیت به True
-
-                # ورود کاربر بعد از تایید
-                login(request, user)
+                user.save()
                 return Response({
-                    'message': 'Account email verified successfully, and you are now logged in.'
+                    'message':'account email verified successfully'
                 }, status=status.HTTP_200_OK)
             return Response({
-                'message': 'Code is invalid, user already verified'
+                'message': 'code is invalid user already verified'
             }, status=status.HTTP_204_NO_CONTENT)
 
         except OTP.DoesNotExist:
             return Response({
-                'message': 'Invalid OTP or OTP not found'
+                'message': 'passcode not provided'
             }, status=status.HTTP_404_NOT_FOUND)
 
-
-class UserLoginView(GenericAPIView):
-    serializer_class = UserLoginSerializer
-
-    def post(self, request):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        # بررسی وضعیت تایید کاربر قبل از ورود
-        user = serializer.validated_data['user']
-        if not user.is_verified:
-            return Response({
-                'message': 'Your email is not verified. Please verify your email first.'
-            }, status=status.HTTP_400_BAD_REQUEST)
-
-        # ورود کاربر پس از تایید
-        login(request, user)
-        return Response({
-            'message': 'Login successful.',
-            'user': {
-                'id': user.id,
-                'username': user.username,
-                'email': user.email
-            }
-        }, status=status.HTTP_200_OK)
 
 class UserLoginView(GenericAPIView):
     serializer_class = UserLoginSerializer
@@ -86,18 +59,34 @@ class UserLoginView(GenericAPIView):
         serializer.is_valid(raise_exception=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
 
-class HomeView(TemplateView):
-    template_name = "home.html"
+class LogoutUserView(GenericAPIView):
+    serializer_class = LogoutUserSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
 
-# class LoginView(TemplateView):
-#      template_name = "login.html"
-
-# class SignupView(TemplateView):
-#      template_name = "signup"
 
 
-class ProfileView(TemplateView):
-    template_name = "profile.html"
+class ProfileView(APIView):
+    permission_classes = [IsOwnerOrReadOnly, ]
 
-# class OTPView(TemplateView):
-#     template_name = "otp.html"
+    def get(self, request,profile_pk):
+        try:
+            profile = Profile.objects.get(pk=profile_pk, user=request.user)
+            self.check_object_permissions(request, profile)
+        except Profile.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+
+    def post(self, request):
+        serializer = ProfileSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
