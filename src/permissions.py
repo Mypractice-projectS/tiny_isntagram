bc8035b7d5bf94d14853883631cd51bf56b86977
parsafk010 <parsafakharian.f33@gmail.com>
2025-03-13 00:06:22 +0330

    feat:creat peormission

--- src/permissions.py
+++ src/permissions.py
@@ -1,12 +1,17 @@
-from rest_framework.permissions import BasePermission,SAFE_METHODS
-
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsOwnerOrReadOnly(BasePermission):
+    """
+    اجازه دسترسی فقط به مالک پروفایل را می‌دهد. در روش‌های خواندنی (GET، HEAD، OPTIONS) همه می‌توانند دسترسی داشته باشند.
+    """
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user
+        # اجازه دسترسی به هر کاربر احراز هویت‌شده
+        return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
+        # اگر درخواست به روش‌های خواندنی باشد (مثل GET، HEAD، OPTIONS)
         if request.method in SAFE_METHODS:
             return True
-        return obj.owner == request.user
+        # در غیر این صورت بررسی مالکیت پروفایل
+        return obj.user == request.user  # بررسی اینکه آیا این پروفایل متعلق به کاربر درخواست‌کننده است
